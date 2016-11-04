7a561e40a75df234baf467704313f9eb70b958b0
cfatrane <cfatrane@student.42.fr>
2016-11-04 11:28:16 +0100

    .

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfatrane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/04 11:27:49 by cfatrane          #+#    #+#             */
+/*   Updated: 2016/11/04 11:27:51 by cfatrane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlen(const char *s)
@@ -6,16 +18,6 @@ size_t	ft_strlen(const char *s)
 
 	i = 0;
 	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t i;
-
-	i = 0;
-	while (*s++)
 		i++;
 	return (i);
 }

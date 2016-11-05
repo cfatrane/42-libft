2946fc6518ca43ee6e341c638cd7aea6ee56adf0
cfatrane <cfatrane@student.42.fr>
2016-11-05 22:07:33 +0100

    .

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -19,6 +19,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str = (char*)malloc(sizeof(*str) * ((ft_strlen(s1) + (ft_strlen(s2)))));
 	if (!str)
 		return (NULL);
-
+	str = ft_strcpy(str, s1);
+	str = ft_strcat(str, s2);
 	return (str);
 }
--- ft_strsub.c
+++ ft_strsub.c
@@ -35,25 +35,3 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	str[i] = '\0';
 	return (str);
 }
-
-/*{
-	char		*str;
-	size_t		i;
-	size_t		t;
-
-	i = 0;
-	t = 0;
-	if (!s)
-		return (NULL);
-	if ((str = (char *)malloc(sizeof(*str) * (n + 1))) == NULL)
-		return (NULL);
-	while (s[start] != '\0' && n > t)
-	{
-		str[i] = s[start];
-		start++;
-		i++;
-		n--;
-	}
-	str[i] = '\0';
-	return (str);
-}*/

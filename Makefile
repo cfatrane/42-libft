a6b9a6e3a19f9804f333d14cfebe4f3d746e563b
cfatrane <cfatrane@student.42.fr>
2016-11-13 15:51:01 +0100

    libft

--- Makefile
+++ Makefile
@@ -68,6 +68,7 @@ SRC =	ft_memset.c		\
 		ft_lstdelone.c	\
 		ft_lstdel.c		\
 		ft_lstadd.c		\
+		ft_lstiter.c	\
 		ft_islower.c	\
 		ft_isupper.c	\
 		ft_isspace.c	\
---
+++ ft_lstiter.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfatrane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/04 13:59:20 by cfatrane          #+#    #+#             */
+/*   Updated: 2016/11/04 13:59:22 by cfatrane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
+{
+
+}
+
+Description Parcourt la liste lst en appliquant à chaque maillon la fonction f.
+Param. #1 Pointeur sur le premier maillon d’une liste.
+Param. #2 L’adresse d’une fonction à laquelle appliquer chaque maillon
+de la liste.
+Retour Rien.
+Fonctions libc Aucune.

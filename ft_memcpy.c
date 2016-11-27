5c11bc71d0e24a3fc3f20e9ad1b7bee013495493
cfatrane <cfatrane@student.42.fr>
2016-11-27 15:09:38 +0100

    libft

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: cfatrane <cfatrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/22 10:37:44 by cfatrane          #+#    #+#             */
-/*   Updated: 2016/11/22 10:37:46 by cfatrane         ###   ########.fr       */
+/*   Updated: 2016/11/27 13:17:52 by cfatrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	oct1 = (unsigned char*)dst;
 	oct2 = (unsigned char*)src;
 	while (n--)
-	{
 		*oct1++ = *oct2++;
-	}
 	return (dst);
 }

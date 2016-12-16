14f730561a83b8cc660fd665ca3b10c6280f56f6
cfatrane <cfatrane@student.42.fr>
2016-12-16 17:06:30 +0100

    libft

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,13 +6,13 @@
 /*   By: cfatrane <cfatrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/16 11:24:12 by cfatrane          #+#    #+#             */
-/*   Updated: 2016/12/16 15:37:56 by cfatrane         ###   ########.fr       */
+/*   Updated: 2016/12/16 16:42:45 by cfatrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_countmalloc_base(unsigned int c, size_t base)
+static size_t	ft_count(unsigned int c, size_t base)
 {
 	size_t i;
 
@@ -25,12 +25,12 @@ size_t	ft_countmalloc_base(unsigned int c, size_t base)
 	return (i);
 }
 
-char	ft_convert_to_char(int c)
+char			ft_convert(int c)
 {
 	if (c > 9)
-		return ('a' + (c - 10));
+		return ((c - 10) + 'a');
 	else
-		return ('0' + c);
+		return (c + '0');
 }
 
 char	*ft_itoa_base(int num, size_t base)
@@ -41,11 +41,12 @@ char	*ft_itoa_base(int num, size_t base)
 	if (base > 36)
 		return (NULL);
 	i = 0;
-	ret = (char*)malloc(sizeof(char) * (ft_countmalloc_base(num, base) + 1));
+	ret = (char*)malloc(sizeof(char) * (ft_count(num, base) + 1));
 	while (num != 0)
 	{
-		ret[i++] = ft_convert_to_char(num % base);
+		ret[i] = ft_convert(num % base);
 		num = num / base;
+		i++;
 	}
 	ret[i] = '\0';
 	return (ft_strrev(ret));

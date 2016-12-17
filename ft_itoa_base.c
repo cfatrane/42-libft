52a204018a99a2950c8d1c7ee5cab97497443579
cfatrane <cfatrane@student.42.fr>
2016-12-17 20:21:08 +0100

    libft

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,48 +6,43 @@
 /*   By: cfatrane <cfatrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/16 11:24:12 by cfatrane          #+#    #+#             */
-/*   Updated: 2016/12/16 16:42:45 by cfatrane         ###   ########.fr       */
+/*   Updated: 2016/12/17 20:19:23 by cfatrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_count(unsigned int c, size_t base)
+static size_t	size_tab(int num, int base)
 {
-	size_t i;
+	size_t	len;
 
-	i = 0;
-	while (c > 1)
-	{
-		c = c / base;
-		i++;
-	}
-	return (i);
-}
-
-char			ft_convert(int c)
-{
-	if (c > 9)
-		return ((c - 10) + 'a');
-	else
-		return (c + '0');
-}
-
-char	*ft_itoa_base(int num, size_t base)
-{		
-	int		i;
-	char	*ret;
-
-	if (base > 36)
-		return (NULL);
-	i = 0;
-	ret = (char*)malloc(sizeof(char) * (ft_count(num, base) + 1));
 	while (num != 0)
 	{
-		ret[i] = ft_convert(num % base);
-		num = num / base;
+		num /= base;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa_base(int num, int base)
+{
+	char	*str;
+	char	*tmp;
+	size_t	i;
+	size_t	len;
+
+	if (num == 0)
+		return (ft_strdup("0"));
+	tmp = ft_strnew(16);
+	strcpy(tmp, "0123456789ABCDEF");
+	len = size_tab(num, base);
+	str = ft_strnew(len);
+	i = 1;
+	while (num != 0)
+	{
+		str[len - i] = tmp[num % base];
+		num /= base;
 		i++;
 	}
-	ret[i] = '\0';
-	return (ft_strrev(ret));
+	return (str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cfatrane <cfatrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/22 10:45:03 by cfatrane          #+#    #+#             */
-/*   Updated: 2016/12/16 15:33:12 by cfatrane         ###   ########.fr       */
+/*   Updated: 2016/12/17 18:14:37 by cfatrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,7 +100,7 @@ size_t				ft_tabintlen(int *tab);
 int					**ft_createtab(int nblin, int nbcol);
 size_t				ft_lstlen(t_list *list);
 int					ft_count_itoa(int num);
-char				*ft_itoa_base(int num, size_t base);
+char				*ft_itoa_base(int num, int base);
 int					ft_count_words_sep(char const *s, char c);
 size_t				ft_strlen_sep(char const *s, char c);
 char				*ft_strrev(char *str);

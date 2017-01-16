30c50eaa8f101b5118bd50abd0827b0a7a567429
cfatrane <cfatrane@student.42.fr>
2017-01-16 15:33:13 +0100

    libft

--- ft_hex_to_dec.c
+++ ft_hex_to_dec.c
@@ -6,7 +6,7 @@
 /*   By: cfatrane <cfatrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 15:02:06 by cfatrane          #+#    #+#             */
-/*   Updated: 2017/01/16 15:29:21 by cfatrane         ###   ########.fr       */
+/*   Updated: 2017/01/16 15:31:45 by cfatrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,16 @@ int	ft_hex_to_dec(char *hex)
 {
 	int		dec;
 	int		temp;
-	int		ismin;
-	int		ismaj;
-	int		isdigit;
 
 	dec = 0;
 	temp = 0;
 	while (*hex != '\0')
 	{
-		ismin = ft_islowhexa(*hex);
-		ismaj = ft_isupphexa(*hex);
-		isdigit = ft_isdigit(*hex);
-		if (ismin)
+		if (ft_islowhexa(*hex))
 			temp = *hex - 'a' + 10;
-		else if (ismaj)
+		else if (ft_isupphexa(*hex))
 			temp = *hex - 'A' + 10;
-		else if (isdigit)
+		else if (ft_isdigit(*hex))
 			temp = *hex - '0';
 		dec = dec * 16 + temp;
 		hex++;
